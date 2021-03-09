afe320e309c93d3c5b15321081455a6ce3ece21f
ongpimkot <79154726+ongpimkot@users.noreply.github.com>
2021-03-09 14:30:56 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,31 @@
+input.onButtonPressed(Button.A, function () {
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    for (let index = 0; index < 4; index++) {
+        pins.servoWritePin(AnalogPin.P2, 50)
+        basic.pause(500)
+        pins.servoWritePin(AnalogPin.P2, 90)
+        basic.pause(500)
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+	
+})
+music.onEvent(MusicEvent.MelodyEnded, function () {
+    pins.digitalWritePin(DigitalPin.P1, 1)
+})
+input.onButtonPressed(Button.B, function () {
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    music.startMelody(music.builtInMelody(Melodies.Birthday), MelodyOptions.Once)
+})
+music.onEvent(MusicEvent.MelodyNotePlayed, function () {
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    basic.pause(200)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    basic.pause(200)
+})
+pins.digitalWritePin(DigitalPin.P1, 1)
+basic.showString("Hello!")
+basic.forever(function () {
+    basic.showIcon(IconNames.Heart)
+    basic.showIcon(IconNames.SmallHeart)
+})
